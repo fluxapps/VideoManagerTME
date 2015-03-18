fda9310e6751d47141cc9a6090d91e51555b2656
Fabian Schmid <fs@studer-raimann.ch>
2015-03-18 08:41:03 +0100

    Public Beta

--- classes/class.ilVideoManagerTMEPlugin.php
+++ classes/class.ilVideoManagerTMEPlugin.php
@@ -6,43 +6,40 @@ include_once("./Services/COPage/classes/class.ilPageComponentPlugin.php");
  *
  * @author Theodor Truffer <tt@studer-raimann.ch>
  */
-class ilVideoManagerTMEPlugin extends ilPageComponentPlugin{
+class ilVideoManagerTMEPlugin extends ilPageComponentPlugin {
 
-    /**
-     * Get plugin name
-     *
-     * @return string
-     */
-    function getPluginName()
-    {
-        return "VideoManagerTME";
-    }
+	/**
+	 * Get plugin name
+	 *
+	 * @return string
+	 */
+	public function getPluginName() {
+		return "VideoManagerTME";
+	}
 
 
-    /**
-     * Get plugin name
-     *
-     * @return string
-     */
-    function isValidParentType($a_parent_type)
-    {
-        return true;
-    }
+	/**
+	 * Get plugin name
+	 *
+	 * @return string
+	 */
+	public function isValidParentType($a_parent_type) {
+		return true;
+	}
 
-    /**
-     * Get Javascript files
-     */
-    function getJavascriptFiles()
-    {
-//        return array("js/pcexp.js");
-    }
 
-    /**
-     * Get css files
-     */
-    function getCssFiles()
-    {
-//        return array("css/pcexp.css");
-    }
+	/**
+	 * Get Javascript files
+	 */
+	public function getJavascriptFiles() {
+		//        return array("js/pcexp.js");
+	}
 
-} 
+
+	/**
+	 * Get css files
+	 */
+	public function getCssFiles() {
+		//        return array("css/pcexp.css");
+	}
+}
