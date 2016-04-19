d2ed1e81aadf838ce446299ea191c96a44485193
theodortruffer <tt@studer-raimann.ch>
2016-04-19 09:35:25 +0200

    update for ILIAS 5.1 (update version & fixed css bug)

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = 'vidm_tme';
 $version = '1.0.0';
 $ilias_min_version = '4.4.0';
-$ilias_max_version = '5.0.999';
+$ilias_max_version = '5.1.999';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'tt@studer-raimann.ch';
 ?>
