610ecaccb5108e0bdbb40318fa232e199215132e
Florian <fw@studer-raimann.ch>
2018-01-19 13:42:49 +0100

    Remove old ILIAS support

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'vidm_tme';
 $version = '1.0.0';
-$ilias_min_version = '4.4.0';
+$ilias_min_version = '5.2.0';
 $ilias_max_version = '5.3.999';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'tt@studer-raimann.ch';
